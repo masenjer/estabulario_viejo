cdd337b69b9c7978237012e3b0a35f5de3491604
masenjer <ralarcon.ochoa@gmail.com>
2016-12-14 16:01:26 +0100

    actualizacion enlace directorio

--- DIVS/MenuCapcalera.php
+++ DIVS/MenuCapcalera.php
@@ -21,7 +21,7 @@ function MostraMenuCapcalera()
         <td width="10px"></td>
         <td class="APeca" ><a href="http://intranet.uab.es" target="_blank"><font color="#d7d8da">Intranet</font></a></td>
         <td width="10px"></td>
-        <td class="APeca"><a href="http://sia.uab.es:8757/cde/control/iniciop?BuscadorOpciones=1&entradaPublica=true&entidad=500&palabras=&query=&idioma=ca&pais=ES&TextCerca=&x=5&y=9" target="_blank"><font color="#d7d8da">Directori</font></a></td>
+        <td class="APeca"><a href="https://siastd.uab.es/pcde/">Directori</font></a></td>
         <td width="20px"></td>
         <td class="APeca" align="left"><font color="#d7d8da">CERCADOR</font><div id="ResultatCerca" style="position:fixed; width:300px; height:90%; overflow:auto; z-index:1000; display:none" align="left" ></div></td>
         <td class="APeca" width="20px"><input type="text" class="fuenteContingut" id="TextCerca" onkeyup="Cercador();" onblur= "$('#ResultatCerca').hide('slow')"/></td>
